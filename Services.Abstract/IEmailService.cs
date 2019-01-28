75e88d3afcffdb9be40699d7040af8f2093996f8
abai <abai2010@bk.ru>
2019-01-28 19:30:57 +0600

    Add project files.

---
+++ Services.Abstract/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Services.Abstract
+{
+    public interface IEmailService
+    {
+        Task SendPlainEmail(string to, string title, string text);
+    }
+}
